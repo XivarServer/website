71f35d6d42f029bdd43cf99c04eb2244c044c0ff
BuildTools <unconfigured@null.spigotmc.org>
2021-05-09 19:22:29 +0200

    Edit members style

--- js/MembersDisplay.js
+++ js/MembersDisplay.js
@@ -23,13 +23,33 @@ class MembersDisplay {
     Object.values(json.members).forEach((member) => {
       const uuid = member.uuid;
 
+      const memberElement = document.createElement('div');
+      memberElement.classList.add('member');
+
+      const imageElement = document.createElement('img');
+      imageElement.classList.add('member-icon');
+      imageElement.src = `https://crafatar.com/avatars/${uuid}`;
+      memberElement.appendChild(imageElement);
+
+      const nameElement = document.createElement('h3');
+      nameElement.classList.add('member-name');
+      memberElement.appendChild(nameElement);
+
+      Object.values(member.ranks).forEach((rankName) => {
+        const rank = json.ranks[rankName];
+
+        const rankElement = document.createElement('p');
+        rankElement.innerHTML = rank.name;
+        rankElement.style.color = rank.color;
+        memberElement.appendChild(rankElement);
+      });
+
+      this.element.appendChild(memberElement);
+
       const url = `https://sessionserver.mojang.com/session/minecraft/profile/${uuid}`;
 
       const request = new XMLHttpRequest();
       request.open('GET', `https://api.allorigins.win/get?url=${encodeURIComponent(url)}`, true);
-
-      const memberElement = document.createElement('div');
-      this.element.appendChild(memberElement);
 
       request.onreadystatechange = () => {
         if (request.readyState !== XMLHttpRequest.DONE) return;
@@ -38,24 +58,7 @@ class MembersDisplay {
         const allOriginsJson = JSON.parse(request.response);
         const mojangJson = JSON.parse(allOriginsJson.contents);
 
-        memberElement.classList.add('member');
-
-        const imageElement = document.createElement('img');
-        imageElement.src = `https://crafatar.com/avatars/${uuid}`;
-        memberElement.appendChild(imageElement);
-
-        const nameElement = document.createElement('h3');
         nameElement.innerHTML = mojangJson.name;
-        memberElement.appendChild(nameElement);
-
-        Object.values(member.ranks).forEach((rankName) => {
-          const rank = json.ranks[rankName];
-
-          const rankElement = document.createElement('p');
-          rankElement.innerHTML = rank.name;
-          rankElement.style.color = rank.color;
-          memberElement.appendChild(rankElement);
-        });
       };
 
       request.send();
